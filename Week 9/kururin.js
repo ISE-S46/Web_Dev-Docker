d3d65e945324d8e8c799ac13183de284bb1046dc
ISE <ise.sns46@gmail.com>
2024-09-05 18:19:58 +0700

    update kururin.js

--- Week 9/kururin.js
+++ Week 9/kururin.js
@@ -1,23 +1,34 @@
-var interval = null;
+var interval = 30;
 var Timer = null;
 
 function kuruInterval() {
-    Timer = window.setInterval(startspin, 30);
+    if (Timer !== null) {
+        window.clearInterval(TTimer); // Clear the previous interval
+    }
+    Timer = window.setInterval(startspin, interval);
 }
+
 function startspin() {
     var pos = $('#Kurukuru').css('left');
     var posnum = parseInt(pos);
-    if (posnum > 1000) {
-        posnum = 50;
+    var windowWidth = $(window).width(); // Get the window width
+    // If image position exceeds the window width, reset to starting point
+    if (posnum > windowWidth - $('#Kurukuru').width()) {
+        posnum = 0; // Reset position to the starting point
     }
     var newpos = posnum + 10 + "px";
     $('#Kurukuru').css('left', newpos);
 }
+
 function Speed() {
-    window.clearInterval(Timer);
-    interval -= 10;
-    Timer = window.setInterval(startspin, interval);
+    if (interval > 10) { // Ensure interval doesn't go below a minimum value
+        interval -= 6;
+        window.clearInterval(Timer); // Clear the existing interval
+        Timer = window.setInterval(startspin, interval);
+    }
 }
+
 function Stop() {
     window.clearInterval(Timer);
+    Timer = null; // Reset the Timer to null to indicate it's stopped
 }
